404f0f73609ad285e37f092247780e81deda1498
Kezia <keziakew98@gmail.com>
2017-11-29 17:36:28 +0800

    tseting

--- demo.js
+++ demo.js
@@ -1,17 +1,16 @@
 var http = require('http');
+var hello = require('./hello');
+hello.world();
 
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello WOrld!');
-}).listen(8080);
-
-/*
-
-var http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
-*/
+http.createServer(function (request, response) {
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+ }).listen(8081);
+ 
+ // Console will print the message
+ console.log('Server running at http://127.0.0.1:8081/');
